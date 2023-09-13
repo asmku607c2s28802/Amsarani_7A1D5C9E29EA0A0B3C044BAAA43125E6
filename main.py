229feb57d92972b15db6aa967f2b622e1b0a33d3
asmku607c2s28802 <144339199+asmku607c2s28802@users.noreply.github.com>
2023-09-13 19:56:00 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
-# Default function to implement conditions to check leap year  
-def CheckLeap(Year):  
-  # Checking if the given year is leap year  
-  if((Year % 400 == 0) or  
-     (Year % 100 != 0) and  
-     (Year % 4 == 0)):   
-    print("Given Year is a leap Year");  
-  # Else it is not a leap year  
-  else:  
-    print ("Given Year is not a leap Year")  
-# Taking an input year from user  
-Year = int(input("Enter the number: "))  
-# Printing result  
-CheckLeap(Year)
+class BankAccount:
+    def __init__(self, account_number, account_holder_name, initial_balance=0):
+        self.__account_number = account_number
+        self.__account_holder_name = account_holder_name
+        self.__account_balance = initial_balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.__account_balance += amount
+            print(f"Deposited ${amount}. New balance: ${self.__account_balance}")
+        else:
+            print("Invalid deposit amount. Amount must be greater than 0.")
+
+    def withdraw(self, amount):
+        if 0 < amount <= self.__account_balance:
+            self.__account_balance -= amount
+            print(f"Withdrew ${amount}. New balance: ${self.__account_balance}")
+        else:
+            print("Invalid withdrawal amount or insufficient balance.")
+
+    def display_balance(self):
+        print(f"Account Balance for {self.__account_holder_name}: ${self.__account_balance}")
+
+
+# Create an instance of the BankAccount class
+my_account = BankAccount("123456789", "John Doe", 1000)
+
+# Test deposit and withdrawal functionality
+my_account.display_balance()
+my_account.deposit(500)
+my_account.withdraw(200)
+my_account.display_balance()
